ebfe420ea73d2e8a7b41487bd21fa2dd6eb27049
haeun <haeun3882@dsm.hs.kr>
2022-04-30 21:13:05 +0900

    [ADD] JavaBoolean

---
+++ JavaBoolean/src/JavaBoolean.java
@@ -0,0 +1,22 @@
+import com.sun.xml.internal.ws.api.ha.StickyFeature;
+
+public class JavaBoolean {
+//    public static void main(String[] args) {
+//        System.out.println(1==2);   //false
+//        System.out.println(1==1);   //true
+//        System.out.println("one"=="two");   //false
+//        System.out.println("one"=="one");   //true
+//    }
+//    public static void main(String[] args) {
+//        System.out.println(1!=2);   //true
+//        System.out.println(1!=1);   //false
+//        System.out.println("one"!="two");   //true
+//        System.out.println("one"!="one");   //false
+//    }
+//    public static void main(String[] args) {
+//        String a = "Hello World";
+//        String b = new String("Hello World");
+//        System.out.println(a == b);
+//        System.out.println(a.equals(b));
+//    }
+}
