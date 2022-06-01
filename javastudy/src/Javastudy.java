a1eb2ce5dcc9da26dc2136d199bf1c8a43c57f99
haeun <haeun3882@dsm.hs.kr>
2022-06-01 22:54:03 +0900

    [ADD] JavaLoop
    자바 생활코딩 반복문

---
+++ javastudy/src/Javastudy.java
@@ -0,0 +1,20 @@
+public class Javastudy {
+
+    public static void main(String[] args) {
+//        int i = 0;
+//
+//        while (i < 10) {
+//            System.out.println("Coding Everybody"+i);
+//            i++;
+//        }
+//        String id = args[0];
+//        if (id.equals("egoing")){
+//            System.out.println("right");
+//        } else {
+//            System.out.println("wrong");
+//        }
+        for (int i = 0; i < 10; i++){
+            System.out.println("Coding Everybody "+i);
+        }
+    }
+}
